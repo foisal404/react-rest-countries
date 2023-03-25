cd6240e2815c4a612ee3275c30574fd6e1509ae4
foisal404 <foisalahmmed2@gmail.com>
2023-03-25 17:13:20 +0600

    file added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p><small>Copyright reserved</small></p>
+        </div>
+    );
+};
+
+export default Footer;
